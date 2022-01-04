e0cb4a2ba2245b218f2f6500ebf38f06aaa6234c
HoangTung17 <tung170100@gmail.com>
2022-01-04 13:22:57 +0700

    ASM1633

---
+++ admin/inc/sidebar.php
@@ -0,0 +1,26 @@
+<div class="grid_2">
+    <div class="box sidemenu">
+        <div class="block" id="section-menu">
+            <ul class="section menu">
+                <li><a>Danh mục sản phẩm</a>
+                    <ul>
+                        <li><a href="catadd.php">Thêm danh mục</a> </li>
+                        <li><a href="catlist.php">Danh Mục</a> </li>
+                    </ul>
+                </li>
+                <li><a>Thương hiệu sản phẩm</a>
+                    <ul >
+                        <li><a href="brandadd.php">Thêm thương hiệu</a> </li>
+                        <li><a href="brandlist.php">Danh sách thương hiệu</a> </li>
+                    </ul>
+                </li>
+                <li><a>Sản phẩm</a>
+                    <ul >
+                        <li><a href="productadd.php">Thêm sản phẩm</a> </li>
+                        <li><a href="productlist.php">Danh sách sản phẩm</a> </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>  
